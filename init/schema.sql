bbfd39342248b312fe7d39174c3d7df61d86eded
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-17 09:29:36 -0300

    feat: Adding file init and .gitignore

---
+++ init/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS seven_deadly_sins_database;
+
+CREATE TABLE IF NOT EXISTS `seven_deadly_sins_database`.`characters` (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    sin VARCHAR(100) NOT NULL,
+    sacred_treasure VARCHAR(100),
+    description VARCHAR(500) NOT NULL,
+    image_base64 MEDIUMTEXT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
