568df3468e6f9062a01149c4bf6c084a3bfeb0d9
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-17 10:47:38 -0300

    feat: Adding connection with Database

---
+++ src/infra/db/connection.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            'root',
+            '123456',
+            'localhost',
+            '3306',
+            'seven_deadly_sins_database'
+        )
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        return create_engine(self.__connection_string)
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
